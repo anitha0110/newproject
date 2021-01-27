ba9127ff8883315595452fc0dfae8c20437c7798
Anitha <anitharajpatel0110@gmail.com>
2021-01-27 11:38:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularProject';
+  count:number;
+  // departments=["computer","physics"];
+  public dHeader = [{ departments: 'computer' }, { departments: 'physics'}, { departments: 'chemistry' }];
+  
+  public Details: object = [];  
+   
+  SearchProduct(departments: string) {  
+    let obj = this.elements.filter(m => m.departments == this.elements);  
+    this.Details = obj;  
+    return this.Details;  
+    }  
+       
+  constructor(){
+    this.getDetails();
+  }
+  
+  getDetails() {  
+          
+    return this.dHeader;  
+ }
+
+ public headElements:any=
+  ['name', 'Age', 'Email ', 'Departments'];
+  
+public elements :any= [
+    {
+      name: "Employee One",
+      age: 40,
+      email: "one@gmail.com",
+      departments: ["Computer", "Physics"],
+    },
+    {
+      name: "Employee Two",
+      age: 10,
+      email: "Two@gmail.com",
+      departments: ["Computer"],
+    },
+    {
+      name: "Employee Three",
+      age: 10,
+      email: "Three@gmail.com",
+      departments: ["Physics", "Chemistry"],
+    },
+    {
+      name: "Employee Four",
+      age: 60,
+      email: "Four@gmail.com",
+      departments: ["Chemistry", "Physics"],
+    },
+    {
+      name: "Employee Five",
+      age: 70,
+      email: "Five@gmail.com",
+      departments: ["Computer", "Physics", "Chemistry"],
+    },
+    {
+      name: "Employee Six",
+      age: 70,
+      email: "Six@gmail.com",
+      departments: ["Computer", "Physics"],
+    },
+    // {
+    //   name: "Employee seven",
+    //   age: 100,
+    //   email: "tom@gmail.com",
+    //   departments: ["MSC"],
+    // },
+  ];
+  // SearchId(name: string) {  
+  //   let obj = this.elements.filter(m => m.Name == name);  
+  //   this.Details = obj;  
+  //   return this.Details;  
+  //   }  
+
+ngOnInit(){
+ this.count=Object.keys(this.elements).length;
+  console.log(Object.keys(this.elements).length);
+}
+
 }
